b415217beb5d8ed386b787e320a4a0150cfb8fda
Jonathan Villalobos Mora <hackertristania@gmail.com>
2021-01-27 00:01:50 +0000

    DOM exe14

--- exercises/11-Add-LI-on-Click/index.js
+++ exercises/11-Add-LI-on-Click/index.js
@@ -2,4 +2,9 @@ let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
 
+	var myUL = document.querySelector("#myList");
+	var addLI = document.createElement("li");
+	addLI.appendChild(document.createTextNode("Fourth Element"));
+	myUL.appendChild(addLI);
 });
+
